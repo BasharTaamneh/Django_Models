7b198caa3d4d6919f010bca6017992a72647b6da
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-16 02:59:29 +0200

    build out models using Django Views and testing templates

---
+++ snack_tracker_project/snacks/tests.py
@@ -0,0 +1,25 @@
+# from django.http import response
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestSnacks(TestCase):
+
+    def test_snack_list_status_code(self):
+        #Arrange
+        url = reverse("snack_list")
+        #Act
+        response = self.client.get(url).status_code
+        #Assert
+        self.assertEqual(response, 200)
+
+
+    def test_snack_list_template(self):
+        #Arrange
+        url = reverse("snack_list")
+        #Act
+        response = self.client.get(url)
+        #Assert
+        self.assertTemplateUsed(response, "snack_list.html")        
+        self.assertTemplateUsed(response, "base.html")
+
